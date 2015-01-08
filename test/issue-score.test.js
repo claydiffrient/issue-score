7139d87818f8dc177bfd405e03b2c86bc7582d55
Clay Diffrient <cdiffrient@instructure.com>
2015-01-07 22:25:09 -0700

    Adds a few tests as well as a travis-ci config

--- test/issue-score.test.js
+++ test/issue-score.test.js
@@ -6,15 +6,42 @@ var sinon = require('sinon');
 
 describe('Issue Score', function () {
 
-  it('should authenticate')
-
-  it('should initialize when given a repository string', function () {
-    var issueSkore = issueScore('claydiffrient/classroom-questions');
-    expect(issueSkore.apiUrl).to.be('https://api.github.com/repos/claydiffrient/classroom-questions/issues/events?per_page=100');
+  it('should throw an error if not given a properly formatted repo', function () {
+    expect(function () {issueScore('notright')}).to.throwError();
   });
 
-  xit('should pull in github credentials from a config file if present', function () {});
+  it('should throw an error if called with only the first part of the repo', function () {
+    expect(function () {issueScore('notokayeither/')}).to.throwError();
+  });
 
+  it('should throw an error if called with only the last part of the repo', function () {
+    expect(function () {issueScore('/notokayeither')}).to.throwError();
+  });
 
+  it('should throw an error if called with no arguments', function () {
+    expect(function () {issueScore()}).to.throwError();
+  });
+
+  it('should throw an error if called with a blank string', function () {
+    expect(function () {issueScore('')}).to.throwError();
+  });
+
+  it('should throw an error if called with a whitespace string', function () {
+    expect(function () {issueScore('      ')}).to.throwError();
+  });
+
+  it('should not throw an error when given a proper repository', function () {
+    expect(function () {issueScore('test/repo')}).to.not.throwError();
+  })
+
+  xdescribe('calculate', function () {
+    it('should calculate the issue scores given repository data', function () {
+      var issueScore = issueScore('test/repo');
+      //TODO: Add mock data here.
+      var data = {};
+      var scoreTable = issueScore.calculate(data)
+      expect(scoreTable).to.be.ok();
+    })
+  })
 
 });
