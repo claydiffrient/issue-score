f429f111a7a21361477cdd0a29c2df405c3ae688
Clay Diffrient <cdiffrient@instructure.com>
2015-01-08 22:09:40 -0700

    Starts refactoring to make a bit better library function.
    - Constructor is almost? complete
    - Tests are around it.

--- test/issue-score.test.js
+++ test/issue-score.test.js
@@ -1,38 +1,48 @@
 "use strict";
 
-var issueScore = require('../lib/issue-score.js');
+var IssueScore = require('../lib/issueScoreRedux.js');
 var expect = require('expect.js');
 var sinon = require('sinon');
 
 describe('Issue Score', function () {
 
-  it('should throw an error if not given a properly formatted repo', function () {
-    expect(function () {issueScore('notright')}).to.throwError();
+  describe('Constructor', function () {
+    it('should throw an error if not given a properly formatted repo', function () {
+      expect(function () {IssueScore('notright')}).to.throwError();
+    });
+
+    it('should throw an error if called with only the first part of the repo', function () {
+      expect(function () {IssueScore('notokayeither/')}).to.throwError();
+    });
+
+    it('should throw an error if called with only the last part of the repo', function () {
+      expect(function () {IssueScore('/notokayeither')}).to.throwError();
+    });
+
+    it('should throw an error if called with no arguments', function () {
+      expect(function () {IssueScore()}).to.throwError();
+    });
+
+    it('should throw an error if called with a blank string', function () {
+      expect(function () {IssueScore('')}).to.throwError();
+    });
+
+    it('should throw an error if called with a whitespace string', function () {
+      expect(function () {IssueScore('      ')}).to.throwError();
+    });
+
+    it('should not throw an error when given a proper repository', function () {
+      expect(function () {IssueScore('test/repo')}).to.not.throwError();
+    });
+
+    it('should properly split the repository string', function () {
+      var issueScore = new IssueScore('testing/special');
+      expect(issueScore.repository).to.be.ok();
+      expect(issueScore.repository.user).to.be('testing');
+      expect(issueScore.repository.repo).to.be('special');
+    })
   });
 
-  it('should throw an error if called with only the first part of the repo', function () {
-    expect(function () {issueScore('notokayeither/')}).to.throwError();
-  });
-
-  it('should throw an error if called with only the last part of the repo', function () {
-    expect(function () {issueScore('/notokayeither')}).to.throwError();
-  });
-
-  it('should throw an error if called with no arguments', function () {
-    expect(function () {issueScore()}).to.throwError();
-  });
-
-  it('should throw an error if called with a blank string', function () {
-    expect(function () {issueScore('')}).to.throwError();
-  });
-
-  it('should throw an error if called with a whitespace string', function () {
-    expect(function () {issueScore('      ')}).to.throwError();
-  });
-
-  it('should not throw an error when given a proper repository', function () {
-    expect(function () {issueScore('test/repo')}).to.not.throwError();
-  })
 
   xdescribe('calculate', function () {
     it('should calculate the issue scores given repository data', function () {
