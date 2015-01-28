ac2596fed3dfeb96daf931283d66902daf52c53c
Clay Diffrient <cdiffrient@instructure.com>
2015-01-27 21:39:05 -0700

    Modifies the install script in another attempt to fix Travis

--- script/ci_install.sh
+++ script/ci_install.sh
@@ -1,6 +1,11 @@
 
 # This is run during Travis CI installs
 
-if [[ $TRAVIS_OS_NAME = "osx" ]]; then
+if [ $TRAVIS_OS_NAME = "osx" ]; then
   brew install nvm
+  npm install
+fi
+
+if [ $TRAVIS_OS_NAME = "linux" ]; then
+  npm install
 fi
